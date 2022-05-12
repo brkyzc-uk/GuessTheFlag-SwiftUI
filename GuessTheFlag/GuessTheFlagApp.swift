3a6ce091115f18d43bb1aa0ea39a86b8a1c8f9ce
brkyzc-uk <burak.yazici@anadolu.edu.tr>
2022-05-12 13:38:21 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Burak YAZICI on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
